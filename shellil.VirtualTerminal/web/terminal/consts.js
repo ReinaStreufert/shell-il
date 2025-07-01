0b43bc5e4a09b6574117934b13cac5480ea625ac
ReinaStreufert <49079389+ReinaStreufert@users.noreply.github.com>
2025-06-30 22:26:23 -0400

    this is how we trip

--- shellil.VirtualTerminal/web/terminal/consts.js
+++ shellil.VirtualTerminal/web/terminal/consts.js
@@ -1,11 +1,11 @@
 ﻿(function () {
     let consts = {
         font: "300 25px inconsolata",
-        defaultBg = "#000000",
-        defaultFg = "#cccccc",
-        blinkInterval = 500,
-        cursorWidth = 2,
-        cursorContrast = 1
+        defaultBg: "#000000",
+        defaultFg: "#cccccc",
+        blinkInterval: 500,
+        cursorWidth: 2,
+        cursorContrast: 1
     };
     window.vtcanvas = {
         constants: consts
